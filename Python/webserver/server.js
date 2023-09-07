e0f87b807875e4f97fae92ffa20a0530b66baea9
JavierMDLH <javimer.25@gmail.com>
2023-09-07 10:40:00 -0500

    *

--- Python/webserver/server.js
+++ Python/webserver/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
 const mysql = require('mysql');
+const dgram = require('dgram'); // Importa el módulo dgram para UDP
 
 const app = express();
 const server = http.createServer(app);
@@ -14,23 +15,59 @@ app.get('/', (req, res) => {
 
 // Conéctate a la base de datos
 const db = mysql.createConnection({
-    host: 'database.cydkk4u9vzj2.us-east-1.rds.amazonaws.com', // Cambia esto con la dirección de tu servidor de base de datos
-    user: 'admin', // Cambia esto con tu nombre de usuario de MySQL
-    password: 'javier3021194', // Cambia esto con tu contraseña de MySQL
-    database: 'proyecto2', // Cambia esto con el nombre de tu base de datos
-  });
+  host: 'database.cydkk4u9vzj2.us-east-1.rds.amazonaws.com',
+  user: 'admin',
+  password: 'javier3021194',
+  database: 'proyecto2',
+});
 
 // Conéctate a la base de datos
 db.connect((err) => {
+  if (err) {
+    console.error('Error al conectar a la base de datos:', err);
+    return;
+  }
+  console.log('Conexión a la base de datos establecida');
+});
+
+// Configura el servidor UDP
+const udpServer = dgram.createSocket('udp4');
+const UDP_PORT = 12345; // Reemplaza con el puerto UDP que deseas utilizar
+
+udpServer.on('listening', () => {
+  const address = udpServer.address();
+  console.log(`Servidor UDP escuchando en ${address.address}:${address.port}`);
+});
+
+udpServer.on('message', (message, remote) => {
+  // Aquí puedes procesar los datos UDP recibidos y luego insertarlos en la base de datos
+  console.log(`Datos UDP recibidos de ${remote.address}:${remote.port}: ${message}`);
+
+  // Separar los datos en latitud, longitud, altitud y hora
+  const received_data = message.toString();
+  const latitud = received_data.split('Latitud ')[1].split(',')[0];
+  const longitud = received_data.split('Longitud ')[1].split(',')[0];
+  const altitud = received_data.split('Altitud ')[1].split(',')[0];
+  const fecha_hora = received_data.split('Hora: ')[1].trim();
+
+  // Ejemplo de inserción en la base de datos
+  const consulta = 'INSERT INTO datos (Latitud, Longitud, Altitud, Timestamp) VALUES (?, ?, ?, ?)';
+  const valores = [latitud, longitud, altitud, fecha_hora];
+
+  db.query(consulta, valores, (err, results) => {
     if (err) {
-      console.error('Error al conectar a la base de datos:', err);
+      console.error('Error al insertar datos en la base de datos:', err);
       return;
     }
-    console.log('Conexión a la base de datos establecida');
+
+    console.log('Datos insertados en la base de datos correctamente.');
   });
-  
-  // Maneja conexiones de clientes Socket.IO
-  io.on('connection', (socket) => {
+});
+
+udpServer.bind(UDP_PORT);
+
+// Resto de tu código de Socket.IO
+io.on('connection', (socket) => {
     console.log('Un cliente se ha conectado');
   
     // Ejemplo: Obtiene datos actualizados de la base de datos cada 5 segundos
@@ -48,38 +85,38 @@ db.connect((err) => {
     socket.on('disconnect', () => {
       console.log('Un cliente se ha desconectado');
     });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Servidor Socket.IO escuchando en el puerto ${PORT}`);
+});
+
+// Función para obtener datos actualizados desde la base de datos
+function obtenerDatosActualizadosDesdeDB(callback) {
+  // Realiza una consulta SQL para obtener tus datos desde la base de datos
+  const consulta = 'SELECT Latitud, Longitud, Altitud, Timestamp FROM datos ORDER BY iddatos DESC LIMIT 1';
+
+  db.query(consulta, (err, results) => {
+    if (err) {
+      callback(err, null);
+      return;
+    }
+
+    if (results.length === 0) {
+      // No se encontraron datos
+      callback(null, []);
+      return;
+    }
+
+    // Extrae los datos de la consulta
+    const data = [
+      results[0].Latitud,
+      results[0].Longitud,
+      results[0].Altitud,
+      results[0].Timestamp,
+    ];
+
+    callback(null, data);
   });
-  
-  const PORT = process.env.PORT || 3000;
-  server.listen(PORT, () => {
-    console.log(`Servidor Socket.IO escuchando en el puerto ${PORT}`);
-  });
-  
-  // Función para obtener datos actualizados desde la base de datos
-  function obtenerDatosActualizadosDesdeDB(callback) {
-    // Realiza una consulta SQL para obtener tus datos desde la base de datos
-    const consulta = 'SELECT Latitud, Longitud, Altitud, Timestamp FROM datos ORDER BY iddatos DESC LIMIT 1';
-  
-    db.query(consulta, (err, results) => {
-      if (err) {
-        callback(err, null);
-        return;
-      }
-  
-      if (results.length === 0) {
-        // No se encontraron datos
-        callback(null, []);
-        return;
-      }
-  
-      // Extrae los datos de la consulta
-      const data = [
-        results[0].Latitud,
-        results[0].Longitud,
-        results[0].Altitud,
-        results[0].Timestamp,
-      ];
-  
-      callback(null, data);
-    });
-  }
+}
