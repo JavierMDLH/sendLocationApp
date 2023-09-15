1805663b826c02d5b314676c746263fd95628faa
JavierMDLH <javimer.25@gmail.com>
2023-09-15 16:53:06 -0500

    *

--- Python/webserver/server.js
+++ Python/webserver/server.js
@@ -5,6 +5,8 @@ const socketIo = require('socket.io');
 const mysql = require('mysql');
 const dgram = require('dgram'); // Importa el módulo dgram para UDP
 
+let mostrarDatosNuevos = false; // Variable para rastrear el estado del interruptor
+
 const app = require('express')(); // Usa require para crear la aplicación express directamente
 const server = http.createServer(app);
 const io = socketIo(server);
@@ -102,14 +104,33 @@ udpServer.bind(UDP_PORT);
 io.on('connection', (socket) => {
   console.log('Un cliente se ha conectado');
 
-  // Obtén los datos actualizados y envíalos cuando un cliente se conecta
-  obtenerDatosActualizadosDesdeDB((err, data) => {
-    if (err) {
-      console.error('Error al obtener datos desde la base de datos:', err);
-      return;
-    }
-    socket.emit('update_data', data);
-  });
+  if (mostrarDatosNuevos){
+    // Obtén los datos actualizados y envíalos cuando un cliente se conecta
+    obtenerDatosActualizadosDesdeDB((err, data) => {
+      if (err) {
+        console.error('Error al obtener datos desde la base de datos:', err);
+        return;
+      }
+      socket.emit('update_data', data);
+    });
+
+  }else{
+
+    socket.on('date_range_selected', (fechas) => {
+      // Obtener datos dentro del rango de fechas especificado
+      obtenerDatosEnRangoDesdeDB(fechas.fechaInicial, fechas.fechaFinal, (err, data) => {
+        if (err) {
+          console.error('Error al obtener datos desde la base de datos:', err);
+          return;
+        }
+  
+        // Enviar datos al cliente
+        socket.emit('update_data', data);
+      });
+    });
+    
+  }
+  
 
   // Maneja la desconexión del cliente
   socket.on('disconnect', () => {
@@ -123,31 +144,56 @@ server.listen(PORT, () => {
 });
 
 // Función para obtener datos actualizados desde la base de datos
-function obtenerDatosActualizadosDesdeDB(callback) {
-  // Realiza una consulta SQL para obtener tus datos desde la base de datos
-  const consulta = 'SELECT Latitud, Longitud, Altitud, Timestamp FROM datos ORDER BY iddatos DESC LIMIT 1';
 
-  db.query(consulta, (err, results) => {
-    if (err) {
-      callback(err, null);
-      return;
-    }
 
-    if (results.length === 0) {
-      // No se encontraron datos
-      callback(null, []);
-      return;
-    }
+function obtenerDatosActualizadosDesdeDB(fechaInicial, fechaFinal, callback) {
+  if (mostrarDatosNuevos) {
+    // Realiza una consulta SQL para obtener tus datos desde la base de datos
+    const consulta = 'SELECT Latitud, Longitud, Altitud, Timestamp FROM datos ORDER BY Timestamp DESC LIMIT 1';
 
-    // Extrae los datos de la consulta
-    const data = [
-      results[0].Latitud,
-      results[0].Longitud,
-      results[0].Altitud,
-      results[0].Timestamp,
-    ];
+    db.query(consulta, (err, results) => {
+      if (err) {
+        callback(err, null);
+        return;
+      }
 
-    callback(null, data);
-    console.log(results[0].Timestamp);
-  });
+      if (results.length === 0) {
+        // No se encontraron datos
+        callback(null, []);
+        return;
+      }
+
+      // Extrae los datos de la consulta
+      const data = [
+        results[0].Latitud,
+        results[0].Longitud,
+        results[0].Altitud,
+        results[0].Timestamp,
+      ];
+
+      callback(null, data);
+      console.log(results[0].Timestamp);
+    });
+  } else {
+    
+    const consulta = 'SELECT Latitud, Longitud, Altitud, Timestamp FROM datos WHERE Timestamp BETWEEN ? AND ? ORDER BY Timestamp';
+    const valores = [fechaInicial, fechaFinal];
+    
+    db.query(consulta, valores, (err, results) => {
+      if (err) {
+        callback(err, null);
+        return;
+      }
+    
+      // Extraer los datos de la consulta
+      const data = results.map((row) => ({
+        latitud: row.Latitud,
+        longitud: row.Longitud,
+        altitud: row.Altitud,
+        timestamp: row.Timestamp,
+      }));
+    
+       callback(null, data);
+    });   
+  }
 }
