188ff7612667051ac02eb5a9091194a8da545e28
JavierMDLH <javimer.25@gmail.com>
2023-09-15 20:53:13 -0500

    *

--- Python/webserver/server.js
+++ Python/webserver/server.js
@@ -196,11 +196,23 @@ function obtenerDatosEnRangoDesdeDB(fechaInicial,fechaFinal,callback){
       latitud: row.Latitud,
       longitud: row.Longitud,
       altitud: row.Altitud,
-      timestamp: row.Timestamp,
+      timestamp: formatDate(row.Timestamp).toLocaleString(),
     }));
     console.log(data[0].timestamp)
 
     callback(null, data);
   });
 }  
+
+//Función para formatear la fecha
+function formatDate(timestamp) {
+  const año = timestamp.substring(4, 8);
+  const mes = timestamp.substring(2, 4);
+  const dia = timestamp.substring(0, 2);
+  const hora = timestamp.substring(8, 10);
+  const minutos = timestamp.substring(10, 12);
+  const segundos = timestamp.substring(12, 14);
+
+  return `${dia}-${mes}-${año} ${hora}:${minutos}:${segundos}`;
+}
 
