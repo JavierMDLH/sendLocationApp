a6d7d437759f839ed72f457f6816e2a206de8b97
JavierMDLH <javimer.25@gmail.com>
2023-09-15 19:12:01 -0500

    *

--- Python/webserver/server.js
+++ Python/webserver/server.js
@@ -70,8 +70,7 @@ udpServer.on('message', (message, remote) => {
   const longitud = received_data.split('Longitud ')[1].split(',')[0];
   const altitud = received_data.split('Altitud ')[1].split(',')[0];
   
-
-  const fecha_hora ="'"+ received_data.split('Hora: ')[1].trim().replace(/[^0-9]/g, '')+"'";
+  const fecha_hora = received_data.split('Hora: ')[1].trim().replace(/[^0-9]/g, '');
 
 
   // Ejemplo de inserción en la base de datos
